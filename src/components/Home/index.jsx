74d6534b52cec2a9b162c318e586cf7f5137b47d
BenCutlerAmes <93072051+BenCutlerAmes@users.noreply.github.com>
2022-10-07 15:21:38 +0100

    home page animated

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,49 @@
 import './index.scss'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters/index'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [
+    'B',
+    'e',
+    'n',
+    ' ',
+    'C',
+    'u',
+    't',
+    'l',
+    'e',
+    'r',
+    '-',
+    'A',
+    'm',
+    'e',
+    's',
+  ]
+  
+
   return (
-    <div className='container home-page'>
-        <div className='text-zone'>
-            <h1>Ben Cutler-Ames</h1>
-            <h2>Software Developer / Career Changer / CodeClan Graduate</h2>
-            <Link to='/contact' className='flat-button'>Contact Me</Link>
-        </div> 
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+        </h1>
+        <h2>
+        Software Developer / CodeClan Graduate / All round good-guy
+        </h2>{' '}
+        <br />
+        <Link to="/contact" className="flat-button">
+          Contact Me
+
+        </Link>
+      </div>
     </div>
   )
 }
---
+++ string_splitter.py
@@ -0,0 +1,7 @@
+string = 'Software Developer / Career Changer / CodeClan Graduate'
+array = []
+
+for letter in string:
+    array.append(letter)
+
+print(array)
